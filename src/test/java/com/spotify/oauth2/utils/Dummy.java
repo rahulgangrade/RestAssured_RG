3fa07b34e3fbbb5fbda6f4159fd2d0eddc666882
unknown <rahul.gangrade@gmail.com>
2021-07-28 21:18:48 +0530

    added m2()

--- src/test/java/com/spotify/oauth2/utils/Dummy.java
+++ src/test/java/com/spotify/oauth2/utils/Dummy.java
@@ -2,8 +2,12 @@ package com.spotify.oauth2.utils;
 
 public class Dummy {
 
-	private void m() {
-		System.out.println("In Method m()");
-		System.out.println("In Method m()");
+	private void m1() {
+		System.out.println("In Method m1()");
+		System.out.println("In Method m1()");
+	}
+	private void m2() {
+		System.out.println("In Method m2()");
+		System.out.println("In Method m2()");
 	}
 }
